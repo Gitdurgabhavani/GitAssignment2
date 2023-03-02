daeaa98dd6d95f849adaeb794b1462eb5ba5963b
Gitdurgabhavni <durgabhavani33312@gmail.com>
2023-03-02 12:39:15 +0530

    Assignment2

---
+++ GitAssignment2.java
@@ -0,0 +1,28 @@
+1.How to check if git is available on your system?
+Ans. Using git version command.
+
+2.How to initialize a new Git repository?
+Ans. Using git init command.
+ 
+3.How to tell git about your name and email?
+Ans. Using git config cammand
+
+4.How to add a file to the staging area?
+Ans. using git add <file-name> command.
+
+5.How to remove a file from the staging area?
+Ans. using git rm-- cached <file-name> command.
+
+6.how to make a commit?
+Ans. using git commit -m <some-messages>
+ 
+7.How to send your changes to a remote repository?
+Ans.  In order to send our changes we need to perform some push operations using some commands
+ i) git branch -M branch name
+ ii)git remote add origin url
+ iii)git push -u origin branch name
+
+8.what is the difference between clone and pull?
+Ans. Git clone is used to clone the repository.
+     Git pull is used to fetch the latest changes made in the remote repository
+ 
